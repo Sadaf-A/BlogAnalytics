597a85e178cd25f66b210d5116bdad9f283cde4a
Sadaf <119438857+Sadaf-A@users.noreply.github.com>
2023-12-09 01:16:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const searchRoutes = require('./routes/SearchRoutes');
 const app = express();
 
 app.use('/api', statsRoutes);
+
 app.use('/api', searchRoutes);
 
 app.listen(8000, () => {console.log("server running on port 8000")});
